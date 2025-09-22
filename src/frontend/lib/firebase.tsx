7d008dd29e2dccd379fa959b7ae32061a8e0c6c0
Anna Gan <annaxjgan@gmail.com>
2025-09-23 00:17:27 +1000

    feat: merged be branch from shared repo

--- src/frontend/lib/firebase.tsx
+++ src/frontend/lib/firebase.tsx
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import Constants from 'expo-constants';
+import {getFirestore} from "@firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: Constants.expoConfig?.extra?.firebaseApiKey,
@@ -14,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
